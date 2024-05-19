49f5f45f2f2dc31d3facac97ea55a665fb2915e0
menglan <52317064+meng-lanbo@users.noreply.github.com>
2024-05-20 00:44:40 +0800

    1

--- src/js/users.js
+++ src/js/users.js
@@ -1,12 +1,12 @@
-const Users=[
-    {"username":"admin","password":"admin","admin":true,"token":""},
-    {"username":"root","password":"root","admin":true,"token":""},
-    {"username":"user1","password":"user1","admin":false,"token":""},
-    {"username":"user2","password":"user2","admin":false,"token":""},
-    {"username":"user3","password":"user3","admin":false,"token":""},
-    {"username":"user3","password":"user3","admin":false,"token":""},
-    {"username":"user4","password":"user4","admin":false,"token":""},
-    {"username":"user5","password":"user5","admin":false,"token":""}
-]
-UserNames=Object.keys(Users).map(value=>value.username)
-export {Users,UserNames}
+const Users = [
+    { "username": "admin", "password": "admin", "admin": true, "token": "" },
+    { "username": "root", "password": "root", "admin": true, "token": "" },
+    { "username": "user1", "password": "user1", "admin": false, "token": "" },
+    { "username": "user2", "password": "user2", "admin": false, "token": "" },
+    { "username": "user3", "password": "user3", "admin": false, "token": "" },
+    { "username": "user3", "password": "user3", "admin": false, "token": "" },
+    { "username": "user4", "password": "user4", "admin": false, "token": "" },
+    { "username": "user5", "password": "user5", "admin": false, "token": "" }
+],
+    UserNames = Object.keys(Users).map(value => value.username)
+export { Users, UserNames }
